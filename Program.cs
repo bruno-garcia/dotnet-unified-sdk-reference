84365ec9429d4abade2d10e8ef48d3bed5f3cd80
Bruno Garcia <bruno@brunogarcia.com>
2018-05-24 20:40:45 +0200

    handling scopes

---
+++ Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Threading.Tasks;
+using Sentry.Protocol;
+using Sentry;
+
+namespace Company.App
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // No SDK is active: Callback should not be invoked!
+            SentrySdk.WithClientAndScope((client, scope) =>
+            {
+                // Create heavy event stuff
+                var evt = new SentryEvent("Don't run me");
+
+                return client.CaptureEvent(evt, scope);
+            });
+
+            // Again, as the SDK is not enabled, callback is never invoked.
+            SentrySdk.ConfigureScope(scope => scope.AddTag(SuperHeavyMethod()));
+            string SuperHeavyMethod() => throw new InvalidOperationException("Don't call me!");
+
+            // Program.Main should be:
+            SentrySdk.Init(o =>
+            {
+                // Some options
+                o.CompressPayload = false;
+                o.ShutdownTimeout = TimeSpan.FromSeconds(5);
+            });
+
+            try
+            {
+                using (SentrySdk.PushScope())
+                {
+                    SentrySdk.ConfigureScope(scope => scope.AddTag("Only visible within App!"));
+
+                    await App();
+
+                    SentrySdk.ConfigureScope(scope => scope.AddTag("Will never be seen at all."));
+                }
+            }
+            catch (Exception exception)
+            {
+                var id = SentrySdk.CaptureException(exception);
+                Console.WriteLine("Id: " + id);
+
+                id = await SentrySdk.CaptureExceptionAsync(exception);
+                Console.WriteLine("Id: " + id);
+            }
+            finally
+            {
+                SentrySdk.CloseAndFlush();
+            }
+
+            // SDK can be reinitialized
+            SentrySdk.Init();
+            // A second call will throw away the previous one and get a new one
+            SentrySdk.Init();
+            try
+            {
+                throw null;
+            }
+            catch (Exception exception)
+            {
+                var id = SentrySdk.CaptureException(exception);
+                Console.WriteLine("Id: " + id);
+            }
+            finally
+            {
+                SentrySdk.CloseAndFlush();
+            }
+
+            // Finally, Closing a disabled SDK is a no-op
+            SentrySdk.CloseAndFlush();
+
+            // Proposed API
+            await ProposedApi();
+        }
+
+        static async Task ProposedApi()
+        {
+            SentrySdk.Init();
+            SentrySdk.ConfigureScope(s => s.AddTag("Some proposed APIs"));
+
+            // if the goal is avoiding execution of the callback when the SDK is disabled
+            // the simplest API is a delegate to create the event
+
+            // Async is best if underlying client is doing I/O (writing event to disk or sending via HTTP)
+            var id = await SentrySdk.CaptureEventAsync(async () =>
+            {
+                // NOT invoked if the SDK is disabled
+                var dbStuff = await DatabaseQuery();
+                return new SentryEvent(dbStuff);
+            });
+            Console.WriteLine("Id: " + id);
+
+            // Blocking alternative (best if using in-memory queue)
+            id = SentrySdk.CaptureEvent(() => new SentryEvent("Nothing async in this callback"));
+            Console.WriteLine("Id: " + id);
+        }
+
+        private static Task<string> DatabaseQuery()
+        {
+            return Task.FromResult("something heavy to retrieve");
+        }
+
+        private static async Task App()
+        {
+            SentrySdk.ConfigureScope(scope => scope.AddTag("Initial scope data."));
+
+            SentrySdk.WithClientAndScope((client, scope) =>
+            {
+                // Create heavy event stuff
+                var evt = new SentryEvent("Entrypoint event.");
+                scope.AddTag("Some scope change done in the callback");
+                return client.CaptureEvent(evt, scope);
+            });
+
+            // TODO: how does it behave with .ConfigureAwait(false);
+            var task = Task.Run(() =>
+            {
+                // If no scope is pushed here, it'd be mutating the outer scope
+                using (SentrySdk.PushScope()) // New scope, clone of the parent
+                {
+                    // Should it be ConfigureNewScope instead and bundle operations?
+                    SentrySdk.ConfigureScope(scope => scope.AddTag("First TPL task adding to scope"));
+
+                    // Simply send event
+                    SentrySdk.CaptureEvent(new SentryEvent("First Event from TPL"));
+                }
+            });
+
+            await task;
+
+            // here we shouldn't see side-effect from the TPL task
+            SentrySdk.CaptureEvent(new SentryEvent("Final event from main thread"));
+
+            throw new Exception("Error in the app");
+        }
+    }
+}
---
+++ Sentry.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Sentry.Protocol;
+using System.Collections.Immutable;
+
+namespace Sentry
+{
+    // TODO: Async
+    // Func to create event only instead of expect u to access the client
+    // consider Serilog's approach
+
+    public static class SentrySdk
+    {
+        private static readonly AsyncLocal<ImmutableStack<Scope>> AsyncLocalScope = new AsyncLocal<ImmutableStack<Scope>>();
+        private static ISentryClient _client;
+
+        internal static ImmutableStack<Scope> ScopeStack
+        {
+            get => AsyncLocalScope.Value ?? (AsyncLocalScope.Value = ImmutableStack.Create(new Scope()));
+            set => AsyncLocalScope.Value = value;
+        }
+
+        internal static void ConfigureScope(Action<Scope> configureScope)
+        {
+            if (_client != null)
+            {
+                var scope = ScopeStack.Peek();
+                configureScope?.Invoke(scope);
+            }
+        }
+
+        // takes options, dsn etc and creates a client:
+        public static void Init(Action<SentryOptions> configureOptions = null)
+        {
+            var options = new SentryOptions();
+            configureOptions?.Invoke(options);
+
+            Interlocked.Exchange(ref _client, new HttpSentryClient(options))
+                .SafeDispose(); // Possibily disposes an old client
+        }
+
+        public static void CloseAndFlush()
+        {
+            // Client should empty it's queue until SentryOptions.ShutdownTimeout
+            Interlocked.Exchange(ref _client, null)
+                .SafeDispose();
+        }
+
+        // Microsoft.Extensions.Logging calls its equivalent method: BeginScope()
+        public static IDisposable PushScope()
+        {
+            var currentScope = ScopeStack;
+            var scope = currentScope.Peek().Clone();
+            var stack = new ScopeSnapshot(currentScope);
+            ScopeStack = currentScope.Push(stack);
+
+            return stack;
+        }
+
+        public static string CaptureEvent(SentryEvent evt)
+            => WithClientAndScope((client, scope)
+                => client.CaptureEvent(evt, scope));
+
+        public static string CaptureException(Exception exception)
+            => WithClientAndScope((client, scope)
+                => client.CaptureException(exception, scope));
+
+        public static Task<string> CaptureExceptionAsync(Exception exception)
+            => WithClientAndScopeAsync((client, scope)
+                => client.CaptureExceptionAsync(exception, scope));
+
+        private const string DisabledSdkResponse = "disabled";
+
+        public static string WithClientAndScope(Func<ISentryClient, Scope, string> handler)
+        {
+            var client = _client;
+            if (client == null)
+            {
+                // some Response object could always be returned while signaling SDK disabled instead of relying on magic strings
+                return DisabledSdkResponse;
+            }
+
+            return handler(client, ScopeStack);
+        }
+
+        public static Task<string> WithClientAndScopeAsync(Func<ISentryClient, Scope, Task<string>> handler)
+        {
+            var client = _client;
+            if (client == null)
+            {
+                // some Response object could always be returned while signaling SDK disabled instead of relying on magic strings
+                return Task.FromResult(DisabledSdkResponse);
+            }
+
+            return handler(client, ScopeStack);
+        }
+
+        #region Proposed API
+
+        public static string CaptureEvent(Func<SentryEvent> eventFactory)
+            => _client?.CaptureEvent(eventFactory(), ScopeStack);
+
+        public static async Task<string> CaptureEventAsync(Func<Task<SentryEvent>> eventFactory)
+        {
+            var client = _client;
+            if (client == null)
+            {
+                // Runs synchronously
+                return DisabledSdkResponse; 
+            }
+
+            // SDK enabled, invoke the factory and the client, asynchronously
+            return await client.CaptureEventAsync(await eventFactory(), ScopeStack.Peek());
+        }
+
+        #endregion
+    }
+
+    internal sealed class ScopeSnapshot : IDisposable
+    {
+        private readonly ImmutableStack<Scope> _snapshot;
+        public ScopeSnapshot(ImmutableStack<Scope> snapshot) => _snapshot = snapshot;
+
+        public void Dispose() => SentrySdk.ScopeStack = _snapshot;
+    }
+
+    // Some SDK options
+    public class SentryOptions
+    {
+        public bool CompressPayload { get; set; } = true;
+        public TimeSpan ShutdownTimeout { get; set; } = TimeSpan.FromSeconds(3);
+    }
+
+    public interface ISentryClient
+    {
+        Task<string> CaptureEventAsync(SentryEvent evt, Scope scope);
+        string CaptureEvent(SentryEvent evt, Scope scope);
+    }
+
+    internal class HttpSentryClient : ISentryClient, IDisposable
+    {
+        public HttpSentryClient(SentryOptions options) { }
+        public Task<string> CaptureEventAsync(SentryEvent evt, Scope scope)
+        {
+            Console.WriteLine("Event captured asynchrounsly: " + evt);
+            return Task.FromResult("321");
+        }
+
+        public string CaptureEvent(SentryEvent evt, Scope scope)
+        {
+            Console.WriteLine($@"Event captured: {evt}
+    Scope: {scope}");
+            return "123";
+        }
+
+        public void Dispose() { }
+    }
+
+    internal class NoOpSentry : ISentryClient
+    {
+        public Task<string> CaptureEventAsync(SentryEvent evt, Scope scope) => throw new InvalidOperationException("Make sure this isn't called");
+        public string CaptureEvent(SentryEvent evt, Scope scope) => throw new InvalidOperationException("Make sure this isn't called");
+    }
+
+    public static class SentryClientExtensions
+    {
+        public static string CaptureException(this ISentryClient client, Exception exception, Scope scope)
+            => client.CaptureEvent(new SentryEvent(exception), scope);
+
+        public static Task<string> CaptureExceptionAsync(this ISentryClient client, Exception exception, Scope scope)
+            => client.CaptureEventAsync(new SentryEvent(exception), scope);
+
+        internal static void SafeDispose(this ISentryClient client) => (client as IDisposable)?.Dispose();
+    }
+}
+
+namespace Sentry.Protocol // testing namespace usage
+{
+    public class SentryEvent
+    {
+        private readonly string _message;
+        public SentryEvent(Exception exception) => _message = exception.Message;
+        public SentryEvent(string message) => _message = message;
+        public override string ToString() => _message;
+    }
+
+    public class Scope
+    {
+        // Could be null in which case default client is used? Is this really going to be used?
+        internal ISentryClient Client { get; set; }
+
+        private IImmutableList<string> _tags = ImmutableList.Create<string>();
+
+        public void AddTag(string tag) => _tags = _tags.Add(tag);
+
+        // Does a deep cloning
+        internal Scope Clone() => new Scope { _tags = _tags.ToImmutableList() };
+        public override string ToString() => string.Join(", ", _tags);
+    }
+}
